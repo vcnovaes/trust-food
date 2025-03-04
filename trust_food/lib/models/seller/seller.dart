843e2b53b85b1579b764a0f2c2adcbd8e2553950
vnovaes <vcn2@cin.ufpe.br>
2025-03-04 01:55:35 -0300

    add data models

---
+++ trust_food/lib/models/seller/seller.dart
@@ -0,0 +1,47 @@
+import 'package:trust_food/models/seller/workScale.dart';
+
+class Seller {
+  final int id;
+  final String userName;
+  final String email;
+  final String phone;
+  final String profileImage;
+  final bool? publicContact;
+  final WorkScale workScale;
+
+  Seller({
+    required this.id,
+    required this.userName,
+    required this.email,
+    required this.phone,
+    required this.profileImage,
+    required this.workScale,
+    this.publicContact
+  });
+
+  // Factory constructor to create a User from JSON
+  factory Seller.fromJson(Map<String, dynamic> json) {
+    return Seller(
+      id: json['id'],
+      userName: json['userName'],
+      email: json['email'],
+      phone: json['phone'],
+      profileImage: json['profileImage'],
+      publicContact: json['publicContact'],
+      workScale: WorkScale.fromJson(json['workScale'])
+    );
+  }
+
+  // Convert a User object to JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'userName': userName,
+      'email': email,
+      'phone': phone,
+      'profileImage': profileImage,
+      'publicContact': publicContact,
+      'workScale': workScale.toJson()
+    };
+  }
+}
---
+++ trust_food/lib/pages/seller/sellerProfile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SellerProfile extends StatelessWidget {
+  const SellerProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+    );
+  }
+}
