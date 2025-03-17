0374f02ac944c57358c90b8e7619fbabdf7d8411
Rebeca Menezes <112202209+RebecaMenezess@users.noreply.github.com>
2025-03-16 21:52:22 -0300

    Update sellerHome.dart

--- trust_food/lib/screens/home/sellerHome.dart
+++ trust_food/lib/screens/home/sellerHome.dart
@@ -1,78 +1,144 @@
 import 'package:flutter/material.dart';
+import 'package:trust_food/screens/qrCode/qrCodeGenerator.dart';
+import 'package:trust_food/screens/seller/sellerMap.dart';
 import 'package:trust_food/models/seller/seller.dart';
 import 'package:trust_food/screens/profile/sellerProfileScreen.dart';
 
 class SellerHomePage extends StatefulWidget {
-  final Seller seller;
-
-  SellerHomePage({required this.seller});
+  const SellerHomePage({super.key});
 
   @override
-  _SellerHomePageState createState() => _SellerHomePageState(seller: seller);
+  _SellerHomePageState createState() => _SellerHomePageState();
 }
 
-enum SellerPage { dashboard, orders, profile }
-
 class _SellerHomePageState extends State<SellerHomePage> {
-  int _selectedIndex = 0;
-
-  final Seller seller;
-
-  _SellerHomePageState({required this.seller});
-
-  static final List<Widget> _sellerPages = <Widget>[
-    Center(child: Text('Seller Dashboard')),
-    Center(child: Text('Orders')),
-    Center(child: Text('Profile')),
-  ];
-
-  void _onItemTapped(int index) {
-    if (_selectedIndex == index) return;
-
-    setState(() {
-      _selectedIndex = index;
-    });
-
-    final selectedPage = SellerPage.values[index];
-
-    switch (selectedPage) {
-      case SellerPage.dashboard:
-        print("Dashboard");
-        break;
-      case SellerPage.orders:
-        print("Orders");
-        break;
-      case SellerPage.profile:
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => SellerProfileScreen(seller: seller),
-          ),
-        );
-        break;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Seller Dashboard")),
-      body: _sellerPages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.dashboard),
-            label: 'Dashboard',
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+        leading: IconButton(
+          icon: Image.asset(
+            'assets/left_arrow.png',
+            height: 24,
+            width: 24,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_bag),
-            label: 'Orders',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ImageSection(
+              image: 'assets/coco_praia.png',
+              wid: 600,
+              hei: 240,
+            ),
+            TitleSection(
+              name: 'Coco do Seu Gustavo',
+              location: 'Praia de Boa Viagem',
+            ),
+            TextSection(
+              description:
+                  'Se você está em busca de uma bebida natural, refrescante e cheia de benefícios para a sua saúde, a água de coco é a escolha perfeita! Além de deliciosa, ela é uma excelente fonte de hidratação e nutrição, ideal para qualquer momento do dia. '
+                  'A água de coco é rica em eletrólitos essenciais, como potássio, magnésio e sódio, que ajudam a manter seu corpo hidratado e cheio de energia. Perfeita para quem pratica esportes, trabalha sob o sol ou simplesmente deseja uma alternativa saudável às bebidas industrializadas. Além disso, é uma ótima opção para ajudar na digestão, melhorar a circulação e fortalecer o sistema imunológico. ',
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SellerMap()),
+                );
+              },
+              child: ImageSection(
+                image: 'assets/Group40.png',
+                wid: 350,
+                hei: 50,
+              ),
+            ),
+            SizedBox(height: 30),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => QRCodeGenerator()),
+                );
+              },
+              child: ImageSection(
+                image: 'assets/QRcodebutton.png',
+                wid: 350,
+                hei: 50,
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({super.key, required this.name, required this.location});
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    name,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(location, style: TextStyle(color: Colors.grey[500])),
+              ],
+            ),
+          ),
+          Icon(Icons.star, color: Color(0xFFF2C305)),
+          const Text('41'),
+        ],
+      ),
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  const TextSection({super.key, required this.description});
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(description, softWrap: true),
+    );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image, required this.wid, required this.hei});
+
+  final String image;
+  final double wid;
+  final double hei;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(image, width: wid, height: hei, fit: BoxFit.cover);
   }
 }
