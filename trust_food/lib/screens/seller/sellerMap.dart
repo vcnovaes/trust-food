8ac945eee2ba6a15d00158e0fb3637f283daf629
Rebeca Menezes <112202209+RebecaMenezess@users.noreply.github.com>
2025-03-16 21:28:09 -0300

    Create sellerMap.dart

---
+++ trust_food/lib/screens/seller/sellerMap.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:trust_food/screens/selection/userModel.dart';
+
+class SellerMap extends StatefulWidget {
+  const SellerMap({super.key});
+
+  @override
+  _SellerMapState createState() => _SellerMapState();
+}
+
+class _SellerMapState extends State<SellerMap> {
+  bool _showCircles = false;
+
+  void _toggleCircles() {
+    setState(() {
+      _showCircles = !_showCircles;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/map_home_user.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            top: 40,
+            left: 16,
+            child: GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: Image.asset(
+                'assets/left_arrow.png',
+                width: 32,
+                height: 32,
+              ),
+            ),
+          ),
+          if (_showCircles) ...[
+            Positioned(
+              top: 200,
+              left: 50,
+              child: Image.asset(
+                'assets/circle.png',
+                width: 100,
+                height: 100,
+              ),
+            ),
+            Positioned(
+              top: 400,
+              right: 50,
+              child: Image.asset(
+                'assets/circle.png',
+                width: 100,
+                height: 100,
+              ),
+            ),
+          ],
+        ],
+      ),
+      bottomNavigationBar: Container(
+        color: Color(0xFFE2EAF0),
+        padding: EdgeInsets.symmetric(vertical: 20),
+        height: 120,
+        child: Center(
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color(0xFF0F5FA6),
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            onPressed: _toggleCircles,
+            child: Text(
+              "Áreas Movimentadas",
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
