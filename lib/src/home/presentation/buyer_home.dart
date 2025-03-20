a981b02bce82479372d8778547611aeecc109465
gbrls <gabrielschneider100@protonmail.com>
2025-03-20 00:52:47 -0300

    added buyer profile drawer

--- lib/src/home/presentation/buyer_home.dart
+++ lib/src/home/presentation/buyer_home.dart
@@ -3,19 +3,114 @@ import 'package:go_router/go_router.dart';
 import 'package:trust_food/src/home/domain/models/user_model.dart';
 import 'package:trust_food/src/qrcode/presentation/qr_code_scanner.dart';
 
+class UserProfileDrawer extends StatelessWidget {
+  final String username;
+  final String? profileImagePath;
+  final VoidCallback onLogout;
+  final List<Widget> menuItems;
+
+  const UserProfileDrawer({
+    super.key,
+    required this.username,
+    this.profileImagePath,
+    required this.onLogout,
+    this.menuItems = const [],
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              _buildProfileHeader(),
+              const Divider(height: 30, color: Colors.grey),
+              _buildMenuList(),
+              _buildLogoutButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  Widget _buildProfileHeader() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundImage: profileImagePath != null
+                ? AssetImage(profileImagePath!)
+                : null,
+            child: profileImagePath == null
+                ? const Icon(Icons.person, size: 30)
+                : null,
+          ),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Text(
+              username,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMenuList() {
+    return Expanded(
+      child: ListView(
+        children: menuItems,
+      ),
+    );
+  }
+
+    Widget _buildLogoutButton() {
+      return Align(
+        alignment: Alignment.bottomRight,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: TextButton(
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.red,
+            ),
+            onPressed: onLogout,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Icon(Icons.logout),
+                const SizedBox(width: 8),
+                const Text("Sair"),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+}
+
 class BuyerHomePage extends StatefulWidget {
   static String route() => '/buyer_home';
-
   const BuyerHomePage({super.key});
-
   @override
   BuyerHomePageState createState() => BuyerHomePageState();
 }
 
 class BuyerHomePageState extends State<BuyerHomePage> {
-  final bool _showAmbulantes = false;
-
-  List<UserModel> users = [
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  bool _showAmbulantes = false;
+  final List<UserModel> users = [
     UserModel(
       email: "gustavo.lins10@gmail.com",
       phone: "99996666",
@@ -32,19 +127,40 @@ class BuyerHomePageState extends State<BuyerHomePage> {
     ),
   ];
 
-  List<UserModel> get recommendedUsers =>
-      users.where((user) => user.userType == "Comerciante").toList();
-
   void _toggleAmbulantes() {
-    // nothing
-    // nothing
-    // nothing
-    // nothing
+    setState(() {
+      _showAmbulantes = !_showAmbulantes;
+    });
+  }
+
+  void _toggleProfileDrawer() {
+    if (_scaffoldKey.currentState!.isEndDrawerOpen) {
+      _scaffoldKey.currentState!.closeEndDrawer();
+    } else {
+      _scaffoldKey.currentState!.openEndDrawer();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Container(),
+        actions: [
+          IconButton(
+            icon: const CircleAvatar(
+              backgroundColor: Color(0xFF0F5FA6),
+              child: Icon(Icons.menu_open_sharp, color: Colors.white),
+            ),
+            onPressed: _toggleProfileDrawer,
+          ),
+        ],
+      ),
       body: Stack(
         children: [
           Positioned.fill(
@@ -52,9 +168,38 @@ class BuyerHomePageState extends State<BuyerHomePage> {
           ),
         ],
       ),
+      endDrawer: UserProfileDrawer(
+        username: "Gildo Come",
+        profileImagePath: 'assets/profile.jpg',
+        onLogout: () => context.go('/login'),
+        menuItems: const [
+          ListTile(
+            leading: Icon(Icons.star, color: Color(0xFF0F5FA6)),
+            title: Text("Minhas Avaliacoes", 
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF0F5FA6),
+                ),
+          ),
+        ),
+          ListTile(
+            leading: Icon(Icons.favorite, color: Color(0xFF0F5FA6)),
+            title: Text("Favoritos", 
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF0F5FA6),
+                ),
+          ),
+        ),
+        ],
+      ),
       bottomNavigationBar: Container(
-        color: Color(0xFFE2EAF0),
-        padding: EdgeInsets.symmetric(vertical: 20),
+        color: const Color(0xFFE2EAF0),
+        padding: const EdgeInsets.symmetric(vertical: 20),
         height: 180,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -65,8 +210,8 @@ class BuyerHomePageState extends State<BuyerHomePage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Image.asset('assets/ambulante.png', width: 24, height: 24),
-                  SizedBox(width: 8),
-                  Text(
+                  const SizedBox(width: 8),
+                  const Text(
                     "Ambulantes por Perto",
                     style: TextStyle(
                       fontFamily: 'Roboto',
@@ -79,22 +224,19 @@ class BuyerHomePageState extends State<BuyerHomePage> {
               ),
             ),
             if (_showAmbulantes) ...[
-              SizedBox(height: 10),
+              const SizedBox(height: 10),
               Expanded(
                 child: ListView.builder(
                   shrinkWrap: true,
-                  itemCount: recommendedUsers.length,
+                  itemCount: users.where((user) => user.userType == "Comerciante").length,
                   itemBuilder: (context, index) {
-                    final user = recommendedUsers[index];
+                    final user = users.where((user) => user.userType == "Comerciante").toList()[index];
                     return Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 16,
-                        vertical: 4,
-                      ),
+                      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
                       child: Text(
                         "${user.firstName} ${user.lastName}",
                         textAlign: TextAlign.center,
-                        style: TextStyle(
+                        style: const TextStyle(
                           fontFamily: 'Roboto',
                           fontSize: 16,
                           fontWeight: FontWeight.w500,
@@ -106,24 +248,22 @@ class BuyerHomePageState extends State<BuyerHomePage> {
                 ),
               ),
             ],
-            SizedBox(height: 12),
+            const SizedBox(height: 12),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF0F5FA6),
-                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                backgroundColor: const Color(0xFF0F5FA6),
+                padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20),
                 ),
               ),
-              onPressed: () {
-                context.go(QRCodeScannerScreen.route());
-              },
+              onPressed: () => context.go(QRCodeScannerScreen.route()),
               child: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   Image.asset('assets/qr_code_1.png', width: 24, height: 24),
-                  SizedBox(width: 8),
-                  Text(
+                  const SizedBox(width: 8),
+                  const Text(
                     "Ler QR Code",
                     style: TextStyle(
                       fontFamily: 'Roboto',
